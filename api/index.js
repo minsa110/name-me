d9faa8e0c57720979df1c2f890cdf230464753c1
Soojin Min <minsa110@gmail.com>
2020-04-27 15:28:06 -0700

    testing reading data from mongo test db

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+// import express from 'express';
 
 /*******************************************************/
 /***** 4. Managing a group of routes using Express *****/
 /*******************************************************/
 
 // --> manage a group of routes in their own module
-const router = express.Router(); // create route object
+// const router = express.Router(); // create route object
 
 // router.get('/', (req, res) => { // this is an api call
 //     res.send({ data: [] }); // so send a json response
@@ -18,7 +18,7 @@ const router = express.Router(); // create route object
 /***** 16. Prepare an API end point to serve the data *****/
 /**********************************************************/
 
-import data from '../src/testData'; // for testing purposes
+// import data from '../src/testData'; // for testing purposes
 
 // router.get('/contests', (req, res) => { // we can get to it with /api/contests
 //     res.send({ contests: data.contests });
@@ -37,28 +37,28 @@ import data from '../src/testData'; // for testing purposes
 // since scanning array to lookup id is inefficient
 // (changing it to object will change it to constant time operation)
 
-const contests = data.contests.reduce((obj, contest) => {
-    obj[contest.id] = contest; // key = contest.id, value = contest itself
-    return obj;
-}, {});
+// const contests = data.contests.reduce((obj, contest) => {
+//     obj[contest.id] = contest; // key = contest.id, value = contest itself
+//     return obj;
+// }, {});
 
-router.get('/contests', (req, res) => {
-    res.send({
-        // use 'reduce' to change array to object
-        // contests: data.contests.reduce((obj, contest) => {
-        //     obj[contest.id] = contest; // key = contest.id, value = contest itself
-        //     return obj;
-        // }, {})
-        // ^ extract this into a variable instead of doing the conversion on the request itself
-        // and put the variable on server load (see 'const contests' above)
-        // so that we do that operation ONCE
-        // now it looks like this:
-        contests: contests
+// router.get('/contests', (req, res) => {
+//     res.send({
+//         // use 'reduce' to change array to object
+//         // contests: data.contests.reduce((obj, contest) => {
+//         //     obj[contest.id] = contest; // key = contest.id, value = contest itself
+//         //     return obj;
+//         // }, {})
+//         // ^ extract this into a variable instead of doing the conversion on the request itself
+//         // and put the variable on server load (see 'const contests' above)
+//         // so that we do that operation ONCE
+//         // now it looks like this:
+//         contests: contests
 
-        // also change the App to read an object instead of an array
-        // doesn't actually 'read' in App --> reads in ContestLists
-    });
-});
+//         // also change the App to read an object instead of an array
+//         // doesn't actually 'read' in App --> reads in ContestLists
+//     });
+// });
 
 // export default router;
 
@@ -67,12 +67,68 @@ router.get('/contests', (req, res) => {
 /***** 23. Fetching contest info from the API *****/
 /**************************************************/
 
-router.get('/contests/:contestId', (req, res) => { // ':/contestId' = dynamic contest ID in express
-    let contest = contests[req.params.contestId];
-    contest.description = 'me fake';
-    // test in browser: http://localhost:8080/api/contests/3
-    res.send(contest);
-    // then, fetch this info with Ajax in ../src/api.js
+// router.get('/contests/:contestId', (req, res) => { // ':/contestId' = dynamic contest ID in express
+//     let contest = contests[req.params.contestId];
+//     contest.description = 'me fake';
+//     // test in browser: http://localhost:8080/api/contests/3
+//     res.send(contest);
+//     // then, fetch this info with Ajax in ../src/api.js
+// });
+
+// export default router;
+
+
+/*****************************************/
+/***** 28. Reading data from MongoDB *****/
+/*****************************************/
+
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import assert from 'assert'; // to make sure we're not getting any errors when we connect
+import config from '../config' // need to import configuration whenever connecting to mongo
+
+// 1.) create mongodb object:
+let mdb;
+MongoClient.connect(config.mongodbUri, { useUnifiedTopology: true }, (err, db) => { // it's a callback
+    assert.equal(null, err); // <-- will raise an error if we have an error
+
+    mdb = db; // assign to global object
+    // 
+});
+
+const router = express.Router();
+
+router.get('/contests', (req, res) => {
+    let contests = {};
+    // 2.) now inside the route, we have access to the mdb connected object
+    // so find all documents in mongo:
+    mdb.db('test').collection('contests').find({}) // gives us a promise
+        .project({ // 3.) only project category and contest names ('1' for the fields we want to be included)
+            id: 1,
+            categoryName: 1,
+            contestName: 1
+        })
+        .each((err, contest) => { // loop ober all the documents (no need to convert to an array since contest = object, != array)
+            assert.equal(null, err);
+
+            if (!contest) { // no more contests to process (end of each loop)
+                res.send({ contests });
+                // ^ return contests as object ('{}') since that's how our React application
+                // is reading the information (i.e. our API structure says contests == list of contests)
+                return;
+            }
+            contests[contest.id] = contest; // add it to empty object created above, since this is async
+        });
+        // test in browser: http://localhost:8080/api/contests (see all contest information)
+});
+
+router.get('/contests/:contestId', (req, res) => {
+    // 4.) find one from database by id
+    mdb.db('test').collection('contests')
+        .findOne({ id: Number(req.params.contestId) }) // convert string into number
+        .then(contest => res.send(contest))
+        .catch(console.error);
+    // test in browser: http://localhost:8080/api/contests/4
 });
 
 export default router;
